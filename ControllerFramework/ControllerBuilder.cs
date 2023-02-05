a691e40bcf05e1f1ea9c608a169b92738e258408
Your Name <you@example.com>
2023-02-04 22:44:06 -0600

    It fits on my screen now!

--- ControllerFramework/ControllerBuilder.cs
+++ ControllerFramework/ControllerBuilder.cs
@@ -18,22 +18,34 @@ public static class ControllerBuilder
 
         var members = type.GetMembers();
 
-        var uiButtons = members.Where(x => x.GetCustomAttributes(typeof(UiButton), false).Length > 0);
-        var uiTextBlocks = members.Where(x => x.GetCustomAttributes(typeof(UiTextBlock), false).Length > 0);
-        var uiTextBoxes = members.Where(x => x.GetCustomAttributes(typeof(UiTextBox), false).Length > 0);
-        var uiCheckBoxes = members.Where(x => x.GetCustomAttributes(typeof(UiCheckBox), false).Length > 0);
-        var uiComboBoxes = members.Where(x => x.GetCustomAttributes(typeof(UiComboBox), false).Length > 0);
-        var uiSliders = members.Where(x => x.GetCustomAttributes(typeof(UiSlider), false).Length > 0);
+        var uiButtons = members
+            .Where(x => x.GetCustomAttributes(typeof(UiButton), false).Length > 0);
+        var uiTextBlocks = members
+            .Where(x => x.GetCustomAttributes(typeof(UiTextBlock), false).Length > 0);
+        var uiTextBoxes = members
+            .Where(x => x.GetCustomAttributes(typeof(UiTextBox), false).Length > 0);
+        var uiCheckBoxes = members
+            .Where(x => x.GetCustomAttributes(typeof(UiCheckBox), false).Length > 0);
+        var uiComboBoxes = members
+            .Where(x => x.GetCustomAttributes(typeof(UiComboBox), false).Length > 0);
+        var uiSliders = members
+            .Where(x => x.GetCustomAttributes(typeof(UiSlider), false).Length > 0);
 
 
         var controllers = new FormController()
         {
-            ButtonControllers = uiButtons.Select(x => new ButtonController(x, obj)).ToArray(),
-            TextBlockControllers = uiTextBlocks.Select(x => new TextBlockController(x, obj)).ToArray(),
-            TextBoxControllers = uiTextBoxes.Select(x => new TextBoxController(x, obj)).ToArray(),
-            CheckBoxControllers = uiCheckBoxes.Select(x => new CheckBoxController(x, obj)).ToArray(),
-            ComboBoxControllers = uiComboBoxes.Select(x => new ComboBoxController(x, obj)).ToArray(),
-            SliderControllers = uiSliders.Select(x => new SliderController(x, obj)).ToArray()
+            ButtonControllers = uiButtons
+                .Select(x => new ButtonController(x, obj)).ToArray(),
+            TextBlockControllers = uiTextBlocks
+                .Select(x => new TextBlockController(x, obj)).ToArray(),
+            TextBoxControllers = uiTextBoxes
+                .Select(x => new TextBoxController(x, obj)).ToArray(),
+            CheckBoxControllers = uiCheckBoxes
+                .Select(x => new CheckBoxController(x, obj)).ToArray(),
+            ComboBoxControllers = uiComboBoxes
+                .Select(x => new ComboBoxController(x, obj)).ToArray(),
+            SliderControllers = uiSliders
+                .Select(x => new SliderController(x, obj)).ToArray()
         };
 
         testTimer.Stop();
@@ -160,7 +172,8 @@ public static class ControllerBuilder
                     if (fieldInfo == null) throw new ArgumentException();
                     if (!fieldInfo.FieldType.IsEnum)
                         throw new Exception("UiComboBox can only be applied to enums");
-                    var enumMembers = fieldInfo.FieldType.GetMembers(BindingFlags.Static | BindingFlags.Public);
+                    var enumMembers = fieldInfo.FieldType
+                        .GetMembers(BindingFlags.Static | BindingFlags.Public);
                     Items = new EnumCbItem[enumMembers.Length];
 
                     for (int i = 0; i < enumMembers.Length; i++)
@@ -187,7 +200,8 @@ public static class ControllerBuilder
                         throw new Exception("UiComboBox can only be applied to enums");
 
 
-                    var enumMembers = propertyInfo.PropertyType.GetMembers(BindingFlags.Static | BindingFlags.Public);
+                    var enumMembers = propertyInfo.PropertyType
+                        .GetMembers(BindingFlags.Static | BindingFlags.Public);
                     Items = new EnumCbItem[enumMembers.Length];
 
                     for (int i = 0; i < enumMembers.Length; i++)
@@ -441,7 +455,8 @@ public static class ControllerBuilder
         {
             if (memberInfo.MemberType != MemberTypes.Method)
                 throw new UiBuilderException(
-                    $"UiButton attribute can only be used on methods. {memberInfo.Name} is not a method.");
+                    $"UiButton attribute can only be used on methods. " +
+                    $"{memberInfo.Name} is not a method.");
         }
     }
 }
